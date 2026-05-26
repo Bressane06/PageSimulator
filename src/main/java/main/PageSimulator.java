c12b884201d8ce905ea303545f58b6a95c310f21
Bressane06 <gatongi44@gmail.com>
2026-05-26 18:50:49 -0300

    initial commit

---
+++ src/main/java/main/PageSimulator.java
@@ -0,0 +1,12 @@
+package main;
+/**
+ *
+ * @author Gabriel Bressane
+ */
+public class PageSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
